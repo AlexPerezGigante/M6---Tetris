991cba37beffc8013e806d7615088c70559fed6a
AlexPerezGigante <perezgigantealex@fpllefia.com>
2024-02-21 02:16:59 +0100

    imagen para render arreglada

--- src/componentes/header.js
+++ src/componentes/header.js
@@ -3,7 +3,7 @@ export const header = {
     `
     <header class="d-flex mx-auto row">
         <div class="col-12 container ">
-        <img src="img/logo.png" alt="logo" width="200" class="mt-5 mx-auto d-block col-1" />
+        <img src="logo.png" alt="logo" width="200" class="mt-5 mx-auto d-block col-1" />
         </div>
         <div class="col-12 w-25 mx-auto row">
         <button class="btn btn-success fs-2 mt-5 col-3 mx-auto" id="botonHome">HOME</button>
